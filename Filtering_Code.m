f05454adf8f408184aef4b355ea0165ccbb6b4de
ThomasSchuster2021 <112416792+ThomasSchuster2021@users.noreply.github.com>
2024-11-12 11:49:53 -0600

    Update Filtering_Code.m

--- Filtering_Code.m
+++ Filtering_Code.m
@@ -2,10 +2,8 @@ close all
 %% Set Limits
 % tic
 img = unw_phase.*mask;
-loopnumber = 3;
-size_limit = 10;
-minPeakValue = 0;
-pixel_lim = 40;
+loopnumber = 5;
+plus_minus = 2;
 % length(img(1,1,:))
 % tic
 % parfor k = 1:51
@@ -36,125 +34,42 @@ end
 
 %% Initilize variable
 imgsbound = img; % Used to determine the number of pixels in the image
-% graycompsave2 = zeros(length(imgsbound), length(imgsbound(:,1))); % holder variable  for blur
-% noiseTest1 = zeros(length(imgsbound), length(imgsbound(:,1))); % holder variable for 3D plots
-% withCellCountSave = zeros(length(imgsbound), length(imgsbound(:,1))); % holder variable for including cell counts
-% grayscaleshow = zeros(length(imgsbound), length(imgsbound(:,1))); % debugging matrix used to see what the final grayscale image looks like
-% bound = zeros(length(imgsbound), length(imgsbound(:,1))); % 
+
 
 peaks = zeros(length(imgsbound), length(imgsbound(:,1))); % Matrix that is used to find the condensates
-% peaksaveList = zeros(length(imgsbound), length(imgsbound(:,1))); % Matrix used to save the full pixels of the condensates
+
 peakLocations = zeros(length(imgsbound)*2,3); % Matrix to store the x, y, and number of peaks for each plane
-% peakLocationAll = zeros(length(imgsbound)*2, 3);
-% peakLocationMatrixAll = zeros(length(imgsbound), length(imgsbound(:,1)));
+
 loc = zeros(length(imgsbound)*3, 2);
 
-% num_peaks_max = 0; % Maximum number of peaks in a plane
-% graytestsave = zeros(length(imgsbound), length(imgsbound(:,1))); % Holder matrix for the graytest matrix (Only if blur)
-% graycompsave = zeros(length(imgsbound), length(imgsbound(:,1))); % Holder matrix for the graycomp matrix
-
 for i=1:length(img(1,1,:))
+    clear pixel_values
 % for i = 1:2
     disp(i + "/" + length(img(1,1,:)))
 %% Grayscale
 %     peakLocations = zeros(length(imgsbound),3); % The locations of the peaks on the current peak
         peaksave = zeros(length(imgsbound), length(imgsbound(:,1))); % 
-        peaksave1 = zeros(length(imgsbound), length(imgsbound(:,1))); % A holder variable for peaksave
-
+        peaksave1 = zeros(length(imgsbound), length(imgsbound(1,:))); % A holder variable for peaksave
+        boundaries_save = ones(length(imgsbound),length(imgsbound(1,:,1)))*255;
         imgs = img(:,:,i); % The specific image being analized 
-%     imgsdoub = im2double(imgs)*255;
-%     if blur
-%         for replay = 1:5
-%         graytest = rgb2gray(imgs);
-%             for x = 2:length(imgs)-1
-%                 for y = 2:length(imgs(:,1))-1
-%                     noise_counter = 0;
-%                     if graytest(x,y) >= graytestHigh
-%                         graytest(x,y) = 255;
-%                     elseif graytest(x,y) <= graytestLow
-%                         graytest(x,y) = 0;
-%                     end
-%                     if graytest(x,y) == 255
-%                         if graytest(x+1,y+1) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x+1,y) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x+1,y-1) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x,y+1) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x,y-1) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x-1,y+1) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x-1,y) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if graytest(x-1,y-1) == 0 || graytest(x,y) == 255
-%                             noise_counter = noise_counter+1;
-%                         end
-%                         if noise_counter == 8
-%                             graytestsave(x,y) = 0;
-%                         else
-%                             graytestsave(x,y) = graytest(x,y);
-%                         end
-%                     else
-%                         graytestsave(x,y) = graytest(x,y);
-%                     end
-%                 end
-%             end
-% 
-%             graytest = graytestsave;
-%         end
-%         for replay = 1:3
-%         for x=2:length(graytest)-1
-%             for y=2:length(graytest(:,1))-1
-%                 graytestsave(x,y) = (graytest(x+1,y+1) + graytest(x+1,y) + graytest(x+1,y-1) +...
-%                                      graytest(x,y+1)   + graytest(x,y)   + graytest(x,y-1) +...
-%                                      graytest(x-1,y+1) + graytest(x-1,y) + graytest(x-1,y-1))/9;
-%                 if graytestsave(x,y)<=graytestLow
-%                     graytestsave(x,y)=0;
-%                 end
-%             end
-%         end
-%         graytest = graytestsave;
-%         grayscalebound = uint8(graytest);
-%     %     end
-%     else
-%         grayscalebound = rgb2gray(imgs);
-%     end
-%     grayscale = rgb2gray(imgs); % Turn the image into Grayscale
         grayscale = imgs;
         graycomp = imcomplement(grayscale); % flip the grayscale bright to dark
-%     for x = 1:length(grayscale)
-%         for y = 1:length(graycomp(:,1))
-%             if grayscale(x,y)>grayscaleMin % If the pixel is brighter than the maximum grayscale complement value, remove the pixel from calculation
-%                 graycomp(x,y)=255;
-%             end
-%             if grayscale(x,y)<=noiseMin % If the pixel is darker than the minimum grayscale complement value, remove the pixel from calculation
-%                 graycomp(x,y)=255;
-%             end
-%             
-%         end
-%     end    
-%     grayscalenew = zeros(length(grayscale),length(grayscale(:,1)));
-%     graycompnew = zeros(length(imgsbound), length(imgsbound(:,1)));
-%     grayscalesave = zeros(length(grayscale),length(grayscale(:,1)));
-%     
-
     
 %% Boundary
         [B, L] = bwboundaries(grayscale, 'holes');
         imgHold = label2rgb(L);
         
         boundaries = rgb2gray(imgHold); % Create a grayscale image of the boundaries
-    
+        
+        for j = 1:250
+            pixel_values(j) = sum(sum(boundaries == j));
+        end
+%         figure
+%         plot(pixel_values)
+        pixel_lim = find(pixel_values == max(pixel_values));
+        pixel_lim_min = pixel_lim-plus_minus;
+        pixel_lim_max = pixel_lim+plus_minus;
+
         % The First and Last rows and columns removed from the boundaries and
         % calculations (would be removed later)
         boundaries(:,1) = 255;
@@ -178,94 +93,67 @@ for i=1:length(img(1,1,:))
 %                 end
 %             end
 %         end
-    
+        
         % noise removal from boundaries
         for loop = 1:loopnumber
             for x = 3:length(grayscale)-2
                 for y = 3:length(grayscale(1,:))-2
                     if (y>2 && y<length(imgsbound)-1 && x>2 && x<length(imgsbound(:,1))-1)
-%                         if (boundaries(x,y+1) == 255 && boundaries(x,y-1) == 255 || boundaries(x-1,y) == 255 && boundaries(x+1,y) == 255 ||...
-%                             boundaries(x,y+2) == 255 && boundaries(x,y-2) == 255 || boundaries(x-2,y) == 255 && boundaries(x+2,y) == 255 ||...
-%                             boundaries(x+1,y+1) == 255 && boundaries(x-1,y-1) == 255 || boundaries(x-1,y+1) == 255 && boundaries(x+1,y-1) == 255)
-%                                 boundaries(x,y) = 255;
+%                         if boundaries(x,y)>pixel_lim_max || boundaries(x,y)<pixel_lim_min
+%                             boundaries_save(x,y)=255;
+%                         else
+%                             boundaries_save(x,y) = pixel_lim;
+%     %                         disp("hi")
 %                         end
-                        if (boundaries(x,y+1)>pixel_lim && boundaries(x,y-1)>pixel_lim || boundaries(x-1,y)>pixel_lim && boundaries(x+1,y)>pixel_lim ||...
-                            boundaries(x,y+2)>pixel_lim && boundaries(x,y-2)>pixel_lim || boundaries(x-2,y)>pixel_lim && boundaries(x+2,y)>pixel_lim ||...
-                            boundaries(x+1,y+1)>pixel_lim && boundaries(x-1,y-1)>pixel_lim || boundaries(x-1,y+1)>pixel_lim && boundaries(x+1,y-1)>pixel_lim)
-                                boundaries(x,y) = 255;
-                        end 
-                        if (boundaries(x,y+1)<pixel_lim && boundaries(x,y-1)<pixel_lim || boundaries(x-1,y)<pixel_lim && boundaries(x+1,y)<pixel_lim ||...
-                            boundaries(x,y+2)<pixel_lim && boundaries(x,y-2)<pixel_lim || boundaries(x-2,y)<pixel_lim && boundaries(x+2,y)<pixel_lim ||...
-                            boundaries(x+1,y+1)<pixel_lim && boundaries(x-1,y-1)<pixel_lim || boundaries(x-1,y+1)<pixel_lim && boundaries(x+1,y-1)<pixel_lim)
-                                boundaries(x,y) = 0;
+%                         boundaries = boundaries_save;
+
+                        if (boundaries(x,y+1)<pixel_lim_min && boundaries(x,y-1)<pixel_lim_min || boundaries(x-1,y)<pixel_lim_min && boundaries(x+1,y)<pixel_lim_min ||...
+                                boundaries(x,y+2)<pixel_lim_min && boundaries(x,y-2)<pixel_lim_min || boundaries(x-2,y)<pixel_lim_min && boundaries(x+2,y)<pixel_lim_min ||...
+                                boundaries(x+1,y+1)<pixel_lim_min && boundaries(x-1,y-1)<pixel_lim_min || boundaries(x-1,y+1)<pixel_lim_min && boundaries(x+1,y-1)<pixel_lim_min)
+                                    boundaries_save(x,y) = 255;
+                        
+                        elseif (boundaries(x,y+1)<=pixel_lim_max && boundaries(x,y-1)<=pixel_lim_max || boundaries(x-1,y)<=pixel_lim_max && boundaries(x+1,y)<=pixel_lim_max ||...
+                                boundaries(x,y+2)<=pixel_lim_max && boundaries(x,y-2)<=pixel_lim_max || boundaries(x-2,y)<=pixel_lim_max && boundaries(x+2,y)<=pixel_lim_max ||...
+                                boundaries(x+1,y+1)<=pixel_lim_max && boundaries(x-1,y-1)<=pixel_lim_max || boundaries(x-1,y+1)<=pixel_lim_max && boundaries(x+1,y-1)<=pixel_lim_max)
+                                    boundaries_save(x,y) = pixel_lim;
+%                                     disp(x + "," + y)
+                        else
+                            boundaries_save(x,y) = 255;
                         end
                     end
                 end
             end
+            
+            boundaries = uint8(boundaries_save);
         end
     
         % If the pixel is outside the cell boundaries set the value to 255
         % (remove from calculations)
         for x=1:length(grayscale)
             for y=1:length(grayscale(1,:))
-                if boundaries(x,y)==0
-                    graycomp(x,y)=graycomp(x,y);
+                if boundaries(x,y)==pixel_lim
+                    graycomp(x,y)=1;
                 else
-                    graycomp(x,y)=255;
+                    graycomp(x,y)=0;
                 end
             end
         end
-      
-%     if blur
-%         for q=1:loopNumber
-%             for x = 1:length(grayscale)
-%                 for y = 1:length(grayscale(1,:))
-%                     if bound(x,y) == 0
-%                         if x >= 5 && x <= length(grayscale)-5
-%                             if y >= 5 && y <= height(grayscale)-5
-%                                 if graycomp(x,y)>0
-%                                     graycompsave2(x,y) = ((graycomp(x+1,y+1) + graycomp(x+1,y) + graycomp(x+1,y-1) + ...
-%                                                          graycomp(x,y+1) + graycomp(x,y-1) + ...
-%                                                          graycomp(x-1,y+1) + graycomp(x-1,y) + graycomp(x-1,y-1))/8+graycomp(x,y))/2;
-%                                 else
-%                                     graycompsave2(x,y) = (graycomp(x+1,y+1) + graycomp(x+1,y) + graycomp(x+1,y-1) + ...
-%                                                           graycomp(x,y+1) + graycomp(x,y-1) + graycomp(x,y) +...
-%                                                           graycomp(x-1,y+1) + graycomp(x-1,y) + graycomp(x-1,y-1))/9;
-%                                 end
-%                             end
-%                         end
-%                     else
-%                         graycompsave2(x,y) = 255;
-%                     end
-%                 end
-%             end
-%             graycomp = graycompsave2;
-%         end
-%         for q=1:2
-%             for x=2:length(graycomp)-1
-%                 for y = 2:length(graycomp(:,1))-1
-%                     blackcount = 0;
-%                     if graycomp(x+1,y) == 0
-%                         blackcount = blackcount+1;
-%                     end
-%                     if graycomp(x,y+1) == 0
-%                         blackcount = blackcount+1;
-%                     end
-%                     if graycomp(x-1,y) == 0
-%                         blackcount = blackcount+1;
-%                     end
-%                     if graycomp(x,y-1) == 0
-%                         blackcount = blackcount+1;
-%                     end
-%                     if blackcount<2
-%                         graycomp(x,y) = 255;
-%                     end
-%                 end
-%             end
-%         end
-%     end
-noise(:,:,i) = img(:,:,i)*double(imcomplement(boundaries))/255;
 
+noise(:,:,i) = abs(img(:,:,i)-double(boundaries)).*graycomp;
+
+    for x = 1:length(noise)
+        for y = 1:length(noise(1,:,1))
+            if noise(x,y,i) == pixel_lim
+                noise(x,y,i) = 1;
+            else
+                noise(x,y,i) = 0;
+            end
+        end
+    end
+    
+    img_noise(:,:,i) = transpose(unw_phase(:,:,i).*noise(:,:,i));
+    
+end
 %% Figures
 figure
 imshow(transpose(boundaries))
@@ -290,19 +178,12 @@ title("img")
 %     figure
 %     imshow(imgs)
 
-%% Functions
-% function array_changed = filter_through_x(img,k)
-%     parfor i = 1:length(img)
-%         img_changed(i,:,k) = filter_through_y(img,k,i)
-%     end
-%     disp(size(img_changed))
-% end
-% 
-% function array_changed = filter_through_y(img,k,i)
-%     parfor j = 1:length(img(1,:,1))
-%         if isnan(img(i,j,k))
-%             img(i,j,k) = 0;
-%         end
-%     end
-%     array_changed = img(i,:,k);
-% end
+
+for x=1:length(img_noise(:,1,12))
+    for y=1:length(img_noise(1,:,12))
+        img_noise(x,y,12) = round(img_noise(x,y,12));
+    end
+end
+for j=1:255
+    img_noise_values(j) = sum(sum(img_noise(:,:,12) == j));
+end
